631ba3887ad01395b84bedc35f0477636ca1bb98
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-24 20:38:26 -0500

    Condicionales en HTML en linea

--- main.js
+++ main.js
@@ -2,22 +2,23 @@ let btn = document.querySelector("div");
 
 btn.addEventListener("click", (e)=>{
     let accion = e.target;
-    if(accion.localName == "button"){
-        if (accion.dataset.opc == "consola") {
-            console.log("Hola soy la opcion de la consola");
-        }else if(accion.dataset.opc == "ventena"){
-            alert("Hola soy la opcion de la ventena");
-        }else if(accion.dataset.opc == "html"){
-            let mensaje = document.querySelector("#mensaje");
-            mensaje.innerHTML = null;
-            mensaje.insertAdjacentHTML("beforeend", "Hola soy la opcion del html");
-        }else{
-            let ventana = document.querySelector("dialog");
-            if(accion.dataset.close){
-                ventana.close();
-            }else{
-                ventana.showModal();
-            }
-        }
+    let html = ()=>{
+        let mensaje = document.querySelector("#mensaje");
+        mensaje.innerHTML = null;
+        mensaje.insertAdjacentHTML("beforeend", "Hola soy la opcion del html");
     }
-}) 
+    let dialog = ()=>{
+        let ventana = document.querySelector("dialog");
+        if(accion.dataset.close) ventana.close();
+        else ventana.showModal();
+    }
+    (accion.localName == "button")
+        ? (accion.dataset.opc == "consola")
+            ? console.log("Hola soy la opcion de la consola")
+            : (accion.dataset.opc == "ventena") 
+                ? alert("Hola soy la opcion de la ventena")
+                : (accion.dataset.opc == "html") 
+                    ? html()
+                    : dialog()
+        : null;
+    }) 
