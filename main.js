a3c17470332a34a69d33bcd905d5bc3b318cf0f4
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-24 20:41:14 -0500

    condicional switch

--- main.js
+++ main.js
@@ -2,23 +2,26 @@ let btn = document.querySelector("div");
 
 btn.addEventListener("click", (e)=>{
     let accion = e.target;
-    let html = ()=>{
-        let mensaje = document.querySelector("#mensaje");
-        mensaje.innerHTML = null;
-        mensaje.insertAdjacentHTML("beforeend", "Hola soy la opcion del html");
+    switch (accion.dataset.opc) {
+        case "consola":
+            console.log("Hola soy la opcion de la consola");
+            break;
+        case "ventana":
+            alert("Hola soy la opcion de la ventana");
+            break;
+        case "html":
+            let mensaje = document.querySelector("#mensaje");
+            mensaje.innerHTML = null;
+            mensaje.insertAdjacentHTML("beforeend", "Hola soy la opcion del html");
+            break;
+        case "emergente":
+            let ventana = document.querySelector("dialog");
+            if(accion.dataset.close) ventana.close();
+            else ventana.showModal();
+            break;
+        default:
+            alert("Opcion no encontrada");
+            break;
     }
-    let dialog = ()=>{
-        let ventana = document.querySelector("dialog");
-        if(accion.dataset.close) ventana.close();
-        else ventana.showModal();
-    }
-    (accion.localName == "button")
-        ? (accion.dataset.opc == "consola")
-            ? console.log("Hola soy la opcion de la consola")
-            : (accion.dataset.opc == "ventena") 
-                ? alert("Hola soy la opcion de la ventena")
-                : (accion.dataset.opc == "html") 
-                    ? html()
-                    : dialog()
-        : null;
-    }) 
+
+})
